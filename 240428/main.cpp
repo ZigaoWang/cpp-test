3ca5d611f7923032528d1e6ae54a353b47eb72bb
ZigaoWang <zigao.wang24@gmail.com>
2024-04-28 16:12:43 +0800

    Auto commit: Changes made on 2024-04-28 at 16:12:43

--- 240428/main.cpp
+++ 240428/main.cpp
@@ -7,6 +7,7 @@ int main() {
     for (int i = 1; i < 5; i++) {
         vi.push_back(i);
     }
+
     vi.insert(vi.begin() + 3, -1);
     vi.erase(vi.begin()+1,vi.begin() + 2);
     for (int i = 0; i < vi.size(); i++) {
