b2b87eaa4ee24ba876736c70f27a4cb189d7c066
ZigaoWang <zigao.wang24@gmail.com>
2024-05-03 22:23:57 +0800

    Update

--- 240428/main.cpp
+++ 240428/main.cpp
@@ -7,11 +7,11 @@ int main() {
     for (int i = 1; i < 5; i++) {
         vi.push_back(i);
     }
-
+    vi.push_back(5);
     vi.insert(vi.begin() + 3, -1);
     vi.erase(vi.begin()+1,vi.begin() + 2);
     for (int i = 0; i < vi.size(); i++) {
-        cout << vi[i] << " ";
+        cout << vi[i] << endl;
     }
     cout << endl;
     return 0;
