f415c0466cc5bb92578527c8655bc43888a4b3b8
ZigaoWang <zigao.wang24@gmail.com>
2024-05-11 16:23:43 +0800

    Update main.cpp

--- 240511/main.cpp
+++ 240511/main.cpp
@@ -15,6 +15,40 @@ int main() {
             text[i] = ' ';
         }
     }
-    
+    // find the most common word
+    string mostCommonWord;
+    int maxCount = 0;
+    int i = 0;
+    while (i < text.size()) {
+        while (i < text.size() && text[i] == ' ') {
+            i++;
+        }
+        int j = i;
+        while (j < text.size() && text[j] != ' ') {
+            j++;
+        }
+        string word = text.substr(i, j - i);
+        int count = 0;
+        for (int k = 0; k < text.size(); k++) {
+            if (text[k] == ' ') {
+                int l = k + 1;
+                while (l < text.size() && text[l] != ' ') {
+                    l++;
+                }
+                string otherWord = text.substr(k + 1, l - k - 1);
+                if (word == otherWord) {
+                    count++;
+                }
+                k = l;
+            }
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            mostCommonWord = word;
+        }
+        i = j;
+    }
+    cout << mostCommonWord << " " << maxCount << endl;
+
     return 0;
 }
