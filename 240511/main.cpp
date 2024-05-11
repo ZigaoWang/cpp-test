4db3d6288e9384c6cc952ead8fa240ef12c39df7
ZigaoWang <zigao.wang24@gmail.com>
2024-05-11 15:08:19 +0800

    Update main.cpp

--- 240511/main.cpp
+++ 240511/main.cpp
@@ -9,28 +9,12 @@ int main() {
     getline(cin, text);
     // convert the text to lowercase
     transform(text.begin(), text.end(), text.begin(), ::tolower);
-    // find the most common word and cout it, with how many times it appeared after it
-    string word;
-    int max = 0;
+    // convert all non-alphabetic characters to spaces
     for (int i = 0; i < text.size(); i++) {
-        if (isalpha(text[i])) {
-            word += text[i];
-        } else {
-            if (word.size() > 0) {
-                int count = 0;
-                for (int j = 0; j < text.size(); j++) {
-                    if (text.substr(j, word.size()) == word) {
-                        count++;
-                    }
-                }
-                if (count > max) {
-                    max = count;
-                }
-                word = "";
-            }
+        if (!isalpha(text[i])) {
+            text[i] = ' ';
         }
     }
-    cout << word << " " << max << endl;
-
+    
     return 0;
 }
