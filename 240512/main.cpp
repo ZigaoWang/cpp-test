10dbfbc9868ac013177844dba7e586be18524034
ZigaoWang <zigao.wang24@gmail.com>
2024-05-12 15:00:43 +0800

    :tada: May 12 2024

--- 240512/main.cpp
+++ 240512/main.cpp
@@ -4,6 +4,9 @@ int main() {
     int a = 10;
     cout << &a << endl; // 取地址符号：16进制
     int *p = &a;
-    cout << *p;
+    cout << *p << endl;
+    cout << &p << endl;
+    int *b = nullptr; // 空指针
+    cout << b; // 输出为 0x0
     return 0;
 }
